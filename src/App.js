c7bc60628a862d31a894db2b45f35d7fb4d8a286
donia-elsayed <dunia2016170569@cis.asu.edu.eg>
2024-09-03 15:02:27 +0300

    add tailwind , images and  app routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import "./App.css";
+import WeatherDashboard from "./components/WeatherDashboard";
+import UserProfile from "./components/UserProfile";
+import SearchHistory from "./components/SearchHistory";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    recentSearches: [],
+  };
+
+  saveRecentSearch = (city) => {
+    this.setState((prevState) => {
+      const recentSearches = [
+        city,
+        ...prevState.recentSearches.filter((search) => search !== city),
+      ].slice(0, 5);
+      localStorage.setItem("recentSearches", JSON.stringify(recentSearches));
+      return { recentSearches };
+    });
+  };
+  render() {
+    return (
+      <div>
+        <nav className="bg-sky-400 text-white p-4">
+          <ul className="container flex justify-evenly gap-6">
+            <li className="font-semibold">
+              <Link to="/">Weather Dashboard</Link>
+            </li>
+            <li className="font-semibold">
+              <Link to="/profile">User Profile</Link>
+            </li>
+            <li className="font-semibold">
+              <Link to="/history">Search History</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <WeatherDashboard saveRecentSearch={this.saveRecentSearch} />
+            }
+          />
+          <Route path="/profile" element={<UserProfile />} />
+          <Route
+            path="/history"
+            element={<SearchHistory searches={this.state.recentSearches} />}
+          />
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
