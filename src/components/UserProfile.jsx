8c348e841dcd50bacc4d00414792060427c5b45b
donia-elsayed <dunia2016170569@cis.asu.edu.eg>
2024-09-03 15:02:59 +0300

    add user profile component

---
+++ src/components/UserProfile.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class UserProfile extends Component {
+  render() {
+    return (
+      <div className="text-3xl font-bold mt-6 text-center text-white">
+        UserProfile
+      </div>
+    );
+  }
+}
